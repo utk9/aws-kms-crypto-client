72f2420686ef7ee05253a6f51f8285513b55912a
utk9 <utkarshlamba.2@gmail.com>
2017-03-23 12:53:35 -0700

    Add method to decrypt

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
- const AWS = require('aws-sdk');
+ const Promise = require('bluebird');
 
+ const config = require('read-config');
+
+const crypto = require('crypto');
+
+ const AWS = require('aws-sdk');
  AWS.config.loadFromPath('./config/aws-config.json');
+ AWS.config.setPromisesDependency(bluebird);
 
  class KMSClient {
  	constructor() {
  		this._client = new aws.KMS({ region: 'us-west-2' });
+ 		this._plainTextDataKey = null;
  	}
+
+ 	decrypt (data) {
+ 		const pPlainTextDataKey = Promise.resolve(this._plainTextDataKey);
+ 		if (!this._plainTextDataKey) {
+ 			const { encryptedDataKey } = config.readConfig('./config.');
+ 			pPlainTextDataKey = this._client.decrypt({ CipherText: Buffer.from(encryptedDataKey, 'base64') }).promise();
+ 		}
+
+ 		return pPlainTextDataKey
+ 			.then((dataKey) => {
+ 				const decipher = crypto.createDecipher('aes256', dataKey);
+ 				let decrypted = decipher.update(data, 'hex', 'utf8');
+ 				decrypted += decipher.final('utf8');
+ 				return decrypted;
+ 			})
+ 			.catch((err) => {
+ 				return null;
+ 			})
+ 	}
+
+ 	flushPlainTextDataKey() {
+ 		this._plainTextDataKey = null;
+ 	}
+
+
  }
